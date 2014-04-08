4fab2d89cebb5b9539c3d0e2d99439fe2be7edac
ahb <andreas.bihlmaier@gmx.de>
2014-04-08 14:40:03 +0200

    zwischendurch

--- scripts/get_relative_tf.py
+++ scripts/get_relative_tf.py
@@ -19,18 +19,10 @@ def main(args):
   tf_listener = tf.TransformListener()
   rospy.sleep(rospy.Duration(0, 500 * 1000))
 
+  try:
+    tf_listener.waitForTransform(to_tf, from_tf, rospy.Time(), rospy.Duration(4.0))
 
-  all_frames_available = False
-  while not all_frames_available:
-    all_frames_available = True
-    for frame in from_tf, to_tf:
-      if not tf_listener.frameExists(frame):
-        print('Frame %s does not exist' % frame)
-        all_frames_available = False
-    rospy.sleep(rospy.Duration(0, 100 * 1000))
-
-  common_time = tf_listener.getLatestCommonTime(from_tf, to_tf)
-  position, quaternion = tf_listener.lookupTransform(from_tf, to_tf, common_time)
+  position, quaternion = tf_listener.lookupTransform(to_tf, from_tf, rospy.Time.now())
   print position, quaternion
 
 
