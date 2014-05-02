c7e28da94be5526652c1f0f6b78919a02e9a38ef
ahb <andreas.bihlmaier@gmx.de>
2014-05-02 15:17:04 +0200

    depend on ahbstring, extend toString for ros TF types

--- include/ahbros.hpp
+++ include/ahbros.hpp
@@ -6,11 +6,55 @@
 #include <iomanip>
 #include <vector>
 
+#include <tf/transform_datatypes.h>
+
+#include <ahbstring.h>
+
 
 namespace ahb {
+namespace string {
+
+/**
+ * Convert tf::Vector3 to string
+ */
+template<> std::string toString<tf::Vector3>(const tf::Vector3& p_vec)
+{
+  std::stringstream ss;
+
+  ss << "(" << p_vec.x() << ", " << p_vec.y() << ", " << p_vec.z() << ")";
+
+  return ss.str();
+}
+
+/**
+ * Convert tf::Quaternion to string
+ */
+template<> std::string toString<tf::Quaternion>(const tf::Quaternion& p_quat)
+{
+  std::stringstream ss;
+
+  ss << "(" << p_quat.x() << ", " << p_quat.y() << ", " << p_quat.z() << ", " << p_quat.w() << ")";
+
+  return ss.str();
+}
+
+/**
+ * Convert tf::Pose to string
+ */
+template<> std::string toString<tf::Pose>(const tf::Pose& p_pose)
+{
+  std::stringstream ss;
+
+  ss << "Pose(Quaternion=" << toString(p_pose.getRotation()) << "; Vector3=" << toString(p_pose.getOrigin()) << ")";
+
+  return ss.str();
+}
+
+} // namespace: string
+
 namespace ros {
 
-
-}} // namespaces
+} // namespace: ros
+} // namespace: ahb
 
 #endif // _AHB_ROS_H_
