50244b07fcaef5b8b5a4fa7588fa0677c15a3972
ahb <andreas.bihlmaier@gmx.de>
2014-06-30 17:21:47 +0200

    https://github.com/ros/urdfdom/tree/master/urdf_parser_py does not work; get joint names manually, nearly finished, see TODO-next

--- scripts/joint_topic_merger.py
+++ scripts/joint_topic_merger.py
@@ -1,32 +1,65 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import roslib
 import rospy
 import sys
 import argparse
-from urdf_parser_py.urdf import URDF
+import xml.etree.ElementTree as ET
 
 from sensor_msgs.msg import JointState
 
-#TODO
-# take any number of sensor_msgs/JointState topics
-# merge them on single topic
-# rename single joints according to one urdf model (i.e. ur5, lwr2 -> or_table_...ur5...lwr2)
 
-class RobotJoints(object):
+
+class Robot(object):
   def __init__(self, id):
     self.id = id
-    self.urdf_string = rospy.get_param('robot_description%d' % self.id, None)
-    if not self.urdf_string:
+    self.source_joint_names = []
+    self.target_joint_names = []
+    self.urdf = rospy.get_param('robot_description%d' % self.id, None)
+    if not self.urdf:
       rospy.logerr('No URDF description for robot %d' % self.id)
       return
-    self.urdf = URDF.from_parameter_server(key='robot_description%d' % self.id)
-    print(self.urdf)
+    self.name = ET.fromstring(self.urdf).attrib['name']
+    rospy.loginfo('id=%d name=%s' % (self.id, self.name))
     self.joint_sub = rospy.Subscriber('joint_states%d' % self.id, JointState, self.on_joint)
 
+
   def on_joint(self, msg):
-    #print(msg)
-    pass
+    if not self.source_joint_names:
+      self.source_joint_names = msg.name
+      rospy.loginfo('%d: source_joint_names: %s' % (self.id, self.source_joint_names))
+      # TODO check against self.urdf
+    if msg.name != self.source_joint_names:
+      rospy.logerr('joint names of robot %d changed (old=%s new=%s), this is not supported. Quitting.' % (self.id, self.source_joint_names, msg.name))
+      sys.exit(1)
+
+    if self.target_joint_names:
+      # TODO-next publish msg with msg.name = self.target_joint_names
+      pass
+
+
+  def get_target_joint_names(self, target_urdf):
+    while not self.source_joint_names:
+      print('Waiting to receive source_joint_names')
+      rospy.sleep(1)
+    root = ET.fromstring(target_urdf)
+    all_target_joint_names = []
+    for joint in root.iter('joint'):
+      all_target_joint_names.append(joint.attrib['name'])
+    rospy.logdebug('all_target_joint_names: %s' % all_target_joint_names)
+    target_joint_names = []
+    for src_name in self.source_joint_names:
+      target_names = [joint_name for joint_name in all_target_joint_names if joint_name.endswith(self.name + '__' + src_name)]
+      if len(target_names) != 1:
+        rospy.logerr('source joint name %s not contained or unique within merged robot: %s. Quitting.' % (src_name, target_names))
+        sys.exit(2)
+      target_joint_names.append(target_names[0])
+    self.target_joint_names = target_joint_names
+    rospy.loginfo('%d: target_joint_names: %s' % (self.id, self.target_joint_names))
+
+
 
 
 def main(args):
@@ -40,16 +73,16 @@ def main(args):
 
   robots = []
   for robot_id in range(robots_count):
-    robots.append(RobotJoints(robot_id))
+    robots.append(Robot(robot_id))
 
   common_urdf = rospy.get_param('robot_description', None)
   if not common_urdf:
     rospy.logerr('No URDF description for common robot')
     return
 
-  # TODO
-  # - parse common_urdf https://github.com/ros/urdfdom/tree/master/urdf_parser_py
-  #   - find joint names of each robot in common robot
+  for robot in robots:
+    robot.get_target_joint_names(common_urdf)
+  
 
   rospy.loginfo('Spinning')
   rospy.spin()
