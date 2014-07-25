58ce876880b2a8cb03890a2d7a9206e495cfe82a
ahb <andreas.bihlmaier@gmx.de>
2014-07-25 14:04:42 +0200

    also output geometry_msgs/Pose

--- scripts/get_relative_tf.py
+++ scripts/get_relative_tf.py
@@ -5,8 +5,33 @@ import rospy
 import sys
 import argparse
 import tf
-import numpy
+import numpy as np
 from tf.transformations import *
+from geometry_msgs.msg import Pose
+
+
+
+def homogeneous2translation_quaternion(homogeneous):
+  """
+  Translation: [x, y, z]
+  Quaternion: [x, y, z, w]
+  """
+  translation = translation_from_matrix(homogeneous)
+  quaternion = quaternion_from_matrix(homogeneous)
+  return translation, quaternion
+
+
+def homogeneous2pose_msg(homogeneous):
+  pose = Pose()
+  translation, quaternion = homogeneous2translation_quaternion(homogeneous)
+  pose.position.x = translation[0]
+  pose.position.y = translation[1]
+  pose.position.z = translation[2]
+  pose.orientation.x = quaternion[0]
+  pose.orientation.y = quaternion[1]
+  pose.orientation.z = quaternion[2]
+  pose.orientation.w = quaternion[3]
+  return pose
 
 
 
@@ -26,8 +51,10 @@ def main(args):
   position, quaternion = tf_listener.lookupTransform(from_tf, to_tf, rospy.Time())
   print('Translation: %s' % str(position))
   print('Quaternion: %s' % str(quaternion))
-  matrix = numpy.dot(translation_matrix(position), quaternion_matrix(quaternion))
+  matrix = np.dot(translation_matrix(position), quaternion_matrix(quaternion))
   print('Matrix:\n%s' % matrix)
+  poseMsg = homogeneous2pose_msg(matrix)
+  print('geometry_msgs/Pose:\n%s' % poseMsg)
 
 
 if __name__ == '__main__':
