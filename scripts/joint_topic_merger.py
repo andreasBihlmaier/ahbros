f8bf41e11c3285db59b43710252eaa47024db237
ahb <andreas.bihlmaier@gmx.de>
2014-08-22 14:35:21 +0200

    print -> rospy.loginfo

--- scripts/joint_topic_merger.py
+++ scripts/joint_topic_merger.py
@@ -45,7 +45,7 @@ class Robot(object):
 
   def get_target_joint_names(self, target_urdf):
     while not self.source_joint_names:
-      print('Waiting to receive source_joint_names')
+      rospy.loginfo('Waiting to receive source_joint_names')
       rospy.sleep(1)
     root = ET.fromstring(target_urdf)
     all_target_joint_names = []
