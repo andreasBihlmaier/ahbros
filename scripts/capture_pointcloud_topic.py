dd10d3b223d293a09b1ed8eea17a5ad25e51b263
ahb <andreas.bihlmaier@gmx.de>
2014-08-29 17:23:36 +0200

    allow to specify tmp dir

--- scripts/capture_pointcloud_topic.py
+++ scripts/capture_pointcloud_topic.py
@@ -13,13 +13,14 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-t', '--topic', default='points', help='Pointcloud topic')
   parser.add_argument('-o', '--output_file', default='/tmp/points.pcd', help='Pointcloud destination file')
+  parser.add_argument('-p', '--pcd_prefix', default='/tmp', help='Prefix to temporarily store pcd files')
   args = parser.parse_args(rospy.myargv()[1:])
 
   rospy.init_node('capture_pointcloud_topic', anonymous=True)
 
   rospy.loginfo('Waiting for pointcloud msg on %s' % args.topic)
   # Set to line buffering mode
-  process_args = ['stdbuf', '--output=L', '--error=L', 'rosrun', 'pcl_ros', 'pointcloud_to_pcd', 'input:=%s' % args.topic, '_prefix:=/tmp/']
+  process_args = ['stdbuf', '--output=L', '--error=L', 'rosrun', 'pcl_ros', 'pointcloud_to_pcd', 'input:=%s' % args.topic, '_prefix:=%s/' % args.pcd_prefix]
   pointcloud_to_pcb_process = subprocess.Popen(process_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   pcd_written = False
   while True:
