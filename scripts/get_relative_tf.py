2976059e737e1bd2214776ac8706f6d70f0a0706
framegrabber <andreas.bihlmaier@gmx.de>
2015-02-23 18:58:24 +0100

    output even more rotation conventions

--- scripts/get_relative_tf.py
+++ scripts/get_relative_tf.py
@@ -8,6 +8,7 @@ import tf
 import numpy as np
 from tf.transformations import *
 from geometry_msgs.msg import Pose
+from math import acos, sqrt
 
 
 
@@ -34,6 +35,34 @@ def homogeneous2pose_msg(homogeneous):
   return pose
 
 
+def homogeneous2quaternion(homogeneous):
+  """
+  Quaternion: [x, y, z, w]
+  """
+  quaternion = quaternion_from_matrix(homogeneous)
+  return quaternion
+
+
+def homogeneous2rpy(homogeneous):
+  """
+  RPY: [sx, sy, sz]
+  """
+  rpy = euler_from_matrix(homogeneous)
+  return rpy
+
+
+def homogeneous2axis_angle(homogeneous):
+  """
+  Axis-angle: [ax, ay, az]
+  """
+  qx, qy, qz, qw = homogeneous2quaternion(homogeneous)
+  angle = 2.0 * acos(qw)
+  norm = sqrt(1 - qw*qw)
+  ax = (qx / norm) * angle
+  ay = (qy / norm) * angle
+  az = (qz / norm) * angle
+  return (ax, ay, az)
+
 
 def main(args):
   parser = argparse.ArgumentParser()
@@ -55,6 +84,10 @@ def main(args):
   print('Matrix:\n%s' % matrix)
   poseMsg = homogeneous2pose_msg(matrix)
   print('geometry_msgs/Pose:\n%s' % poseMsg)
+  rpy = homogeneous2rpy(matrix)
+  print('RPY: %s' % str(rpy))
+  axis_angle = homogeneous2axis_angle(matrix)
+  print('Axis Angle: %s' % str(axis_angle))
   print('As static publisher: rosrun tf static_transform_publisher  %s  %s  %s %s 100' % (' '.join([str(p) for p in position]), ' '.join([str(q) for q in quaternion]), from_tf, to_tf))
 
 
